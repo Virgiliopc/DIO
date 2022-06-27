2b593fb1a15826db9960f2a5f65ea31c024e879f
Virgílio Pires da Costa <virgilio.pires.costa@gmail.com>
2022-06-27 20:58:16 -0300

    "removing items from other projects"

--- cities-api-main/src/main/java/com/github/rickleite/citiesapi/countries/repository/CountryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.github.rickleite.citiesapi.countries.repository;
-
-import com.github.rickleite.citiesapi.countries.Country;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CountryRepository extends JpaRepository<Country, Long> {
-}
--- cities-api-main/src/main/java/com/github/rickleite/citiesapi/states/StateRepository.java
+++
@@ -1,6 +0,0 @@
-package com.github.rickleite.citiesapi.states;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StateRepository extends JpaRepository<State, Long> {
-}
